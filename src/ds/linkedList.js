3a2a03945f326b505364048cb1dace5d0b26fe7e
Shahid Khan <mohammedshahid.khan@broadcom.com>
2020-08-11 10:51:14 -0500

    linkedList push

---
+++ src/ds/linkedList.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if(this.isEmpty) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+
+  }
+}
